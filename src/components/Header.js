e73da25b5ac3f157c48815523c1c868dd0f926f8
celilcanidiz <celilcanidiz@gmail.com>
2022-08-30 10:09:56 +0300

    5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Navbar } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 function Header() {
   return (
+    <Navbar sticky="top">
     <Container className='header'>
         <Row className='mx-auto'>
     <Col md={{ span: 2, offset: 5 }}><Image src='https://neal.fun/spend/billgates.jpg' roundedCircle className='profile_img' /></Col>
@@ -15,6 +17,7 @@ function Header() {
             </Col>
         </Row>
      </Container>
+     </Navbar>
   )
 }
 
