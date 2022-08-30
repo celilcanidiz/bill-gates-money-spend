102f04d504f891d135898f8a09bd853912241d5c
celilcanidiz <celilcanidiz@gmail.com>
2022-08-30 10:16:38 +0300

    son

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Navbar } from 'react-bootstrap';
+
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 function Header() {
   return (
-    <Navbar sticky="top">
-    <Container className='header'>
+  <Container className='header'>
         <Row className='mx-auto'>
     <Col md={{ span: 2, offset: 5 }}><Image src='https://neal.fun/spend/billgates.jpg' roundedCircle className='profile_img' /></Col>
         </Row>
@@ -17,7 +16,6 @@ function Header() {
             </Col>
         </Row>
      </Container>
-     </Navbar>
   )
 }
 
--- src/components/MoneyCounter.js
+++ src/components/MoneyCounter.js
@@ -5,11 +5,13 @@ import NumberFormat from 'react-number-format';
 function MoneyCounter() {
   const money = useSelector(state => state.product.money);
   return (
-    <Container>
+    
+    <Container className='sticky-top'>
         <Row style={{backgroundColor:'#48BB78', color:'white'}}>
             <h1><NumberFormat value={money} displayType='text' thousandSeparator={true} prefix={'$'} /></h1>
         </Row>
     </Container>
+    
   )
 }
 
